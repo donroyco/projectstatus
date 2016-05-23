4a9372f60cb9a80acbce054899e9cdf9ba7af0fe
Peter Welp <peter.welp@klm.com>
2016-05-23 17:42:47 +0200

    separate module

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+app = express();
+server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(8080);
+app.use(express.static('public'));		
+
+var projectStatus = require('./projectstatus');
+
+var bambooStatus = 'unknown';
+var serverStatus = 'unknown';
+
+var ticker = setInterval(function() {
+	var statusMessage = 'bamboo: ' + 
+			    		bambooStatus + 
+                        ' health: ' + 
+                        serverStatus;
+	console.log(statusMessage);
+	io.sockets.emit('status', {value: statusMessage});	
+
+	projectStatus.setBambooStatus(bambooStatus);
+	if (bambooStatus === 'failed' || serverStatus === 'down') {
+		projectStatus.ringBell();
+	}
+	projectStatus.setHealthStatus(serverStatus);
+
+}, 5000);
+
+io.sockets.on('connection', function (socket) {
+	socket.on('bamboo', function (data) {
+		console.log("bamboo found " + data.value);
+		bambooStatus = data.value;
+		//io.sockets.emit('status', {value: 'bamboo status changed'});	
+	}); 
+	socket.on('backend', function (data) {
+		console.log("backend found " + data.value);
+		serverStatus = data.value;
+		//io.sockets.emit('status', {value: 'backend status changed'});	
+	}); 
+	socket.on('quitAll', function (data) {
+		console.log("quitting");
+		projectStatus.allOff();
+		
+		io.sockets.emit('led', {value: 'off'});	
+		process.exit();
+	});
+	
+	socket.emit('status', {value: 'listening'});
+});
+
+console.log("running");
