c1ea0f7c4bd998fd2bfc94fbc3e017554840a31e
PtrWlp <peterendewelp@gmail.com>
2016-06-05 19:27:17 +0200

    better frontend

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ app = express();
 server = require('http').createServer(app);
 
 var io = require('socket.io').listen(server);
-server.listen(8080);
+server.listen(8081);
 app.use(express.static('public'));		
 
-var projectStatus = require('./projectstatususb');
+var projectStatus = require('./projectstatus');
 
 projectStatus.allOff();
 
-process.exit(); 
+// process.exit(); 
 
 // that was quick
 
@@ -23,25 +23,28 @@ var ticker = setInterval(function() {
 
 	update();
 
-}, 20000);
+}, 5000);
 
 
 function update () {
 
-	bambooStatus = bambooStatusService.getBambooStatus();
+	//bambooStatus = bambooStatusService.getBambooStatus();
 
 	var statusMessage = 'bamboo: ' + 
-			    		bambooStatus + 
+			    bambooStatus + 
                         ' health: ' + 
                         serverStatus;
 	console.log(statusMessage);
-	io.sockets.emit('status', {value: statusMessage});	
+//	io.sockets.emit('status', {value: statusMessage});	
 
 	projectStatus.setBambooStatus(bambooStatus);
 	if (bambooStatus === 'failed' || serverStatus === 'down') {
 		//projectStatus.ringBell();
 	}
 	projectStatus.setHealthStatus(serverStatus);
+
+	io.sockets.emit('lightBamboo', {value: bambooStatus});	
+	io.sockets.emit('lightBackend', {value: serverStatus});	
 };
 
 
@@ -49,18 +52,17 @@ io.sockets.on('connection', function (socket) {
 	socket.on('bamboo', function (data) {
 		console.log("bamboo found " + data.value);
 		bambooStatus = data.value;
-		//io.sockets.emit('status', {value: 'bamboo status changed'});	
 	}); 
 	socket.on('backend', function (data) {
 		console.log("backend found " + data.value);
 		serverStatus = data.value;
-		//io.sockets.emit('status', {value: 'backend status changed'});	
 	}); 
 	socket.on('quitAll', function (data) {
 		console.log("quitting");
 		projectStatus.allOff();
 		
-		io.sockets.emit('led', {value: 'off'});	
+		io.sockets.emit('led', {value: 'off'});
+		server.close();	
 		process.exit();
 	});
 	
