647e924eccbfafceb0910ed2756c60b455da64a9
PtrWlp <peterendewelp@gmail.com>
2016-06-19 16:50:45 +0200

    click

--- statusLamp.js
+++ statusLamp.js
@@ -4,6 +4,7 @@ var StatusLamp = function (startPort) {
     this.lampRed = startPort;
     this.lampGreen = startPort +1;
     this.lampBlue = startPort +2;
+    this.currentColor = 'off';
   }
 
 // properties and methods
@@ -11,48 +12,51 @@ StatusLamp.prototype = {
     set: function(color) {
         // Somehow use a C process to set the color
         let lColor = color.toLowerCase();
-        setColor(this.lampRed, 'off');
-        setColor(this.lampGreen, 'off');
-        setColor(this.lampBlue, 'off');
-
-        switch (lColor) {
-               case 'red': 
-                    setColor(this.lampRed, 'on');
-               break;
-               case 'green': 
-                    setColor(this.lampGreen, 'on');
-               break;
-               case 'blue': 
-                    setColor(this.lampBlue, 'on');
-               break;
-               case 'yellow': 
-                    setColor(this.lampRed, 'on');
-                    setColor(this.lampGreen, 'on');
-               break;
-               case 'purple': 
-                    setColor(this.lampRed, 'on');
-                    setColor(this.lampBlue, 'on');
-               break;
-               case 'sea': 
-                    setColor(this.lampGreen, 'on');
-                    setColor(this.lampBlue, 'on');
-               break;
-               case 'all': 
-                    setColor(this.lampRed, 'on');
-                    setColor(this.lampGreen, 'on');
-                    setColor(this.lampBlue, 'on');
-               break;
-               case 'off': 
-                    setColor(this.lampRed, 'off');
-                    setColor(this.lampGreen, 'off');
-                    setColor(this.lampBlue, 'off');
-               break;
-               default:  console.log('unknown color: ' , color);
-        }
+        if (color !== this.currentColor) {
+		this.currentColor = color;
+	        setColor(this.lampRed, 'off');
+	        setColor(this.lampGreen, 'off');
+	        setColor(this.lampBlue, 'off');
+	
+	        switch (lColor) {
+	               case 'red': 
+	                    setColor(this.lampRed, 'on');
+	               break;
+	               case 'green': 
+	                    setColor(this.lampGreen, 'on');
+	               break;
+	               case 'blue': 
+	                    setColor(this.lampBlue, 'on');
+	               break;
+	               case 'yellow': 
+	                    setColor(this.lampRed, 'on');
+	                    setColor(this.lampGreen, 'on');
+	               break;
+	               case 'purple': 
+	                    setColor(this.lampRed, 'on');
+	                    setColor(this.lampBlue, 'on');
+	               break;
+	               case 'sea': 
+	                    setColor(this.lampGreen, 'on');
+	                    setColor(this.lampBlue, 'on');
+	               break;
+	               case 'all': 
+	                    setColor(this.lampRed, 'on');
+	                    setColor(this.lampGreen, 'on');
+	                    setColor(this.lampBlue, 'on');
+	               break;
+	               case 'off': 
+	                    setColor(this.lampRed, 'off');
+	                    setColor(this.lampGreen, 'off');
+	                    setColor(this.lampBlue, 'off');
+	               break;
+	               default:  console.log('unknown color: ' , color);
+	        }
+	}
 
         function setColor(lampColor, state) {
             // Do the exec
-            var command = `../crelay/src/crelay ${lampcolor} ${state}`;
+            var command = `../crelay/src/crelay ${lampColor} ${state}`;
 
             var exec = require('child_process').exec;
             exec(command, function(error, stdout, stderr) {
